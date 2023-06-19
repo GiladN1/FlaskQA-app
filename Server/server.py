b122b7353d8a84305989d69b640dc03b1070dfab
GiladN1 <gilad.nir11@gmail.com>
2023-06-19 19:39:12 +0300

    Created initial route, templates folder and index.html
    closes #2

--- Server/server.py
+++ Server/server.py
@@ -1,2 +1,9 @@
 from flask import Flask, render_template, request, jsonify
 from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("index.html")
